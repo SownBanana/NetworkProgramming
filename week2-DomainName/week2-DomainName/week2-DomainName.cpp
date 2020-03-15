ce676acb6e15f3e6ad04a11fb7e5bd374fedf0b5
Pham Hong Son 20173344 <son.ph173344@sis.hust.edu.vn>
2020-03-15 23:15:31 +0700

    fix bug

--- week2-DomainName/week2-DomainName/week2-DomainName.cpp
+++ week2-DomainName/week2-DomainName/week2-DomainName.cpp
@@ -3,11 +3,11 @@
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <string>
-
 
 using namespace std;
 
@@ -24,18 +24,14 @@ int main()
     SOCKADDR_IN addr;
 
     int count = 1;
-    //string input;
-    char* domains = new char[500];
+    char* domains = new char[1000];
     char* domain;
     bool check_domain = true;
     bool check_dot = false;
 
     cout << "Enter Some Domain Names (separate by space): ";
-    //getline(cin, input);
-    cin.getline(domains, 500);
-    //string to char*
-    
-    //strcpy(domains, input.c_str());
+    cin.getline(domains, 1000);
+    //Tach char array
     domain = strtok(domains, " ");
     //Kiem tra ten mien hop le
     cout << "Checking Entered Domain Names..."<<endl;
@@ -54,13 +50,14 @@ int main()
             }
         }
         if (!check_domain || !check_dot) {
-            cout << "Not Valid" << endl;
+            cout << "Invalid" << endl;
             cout << "================================" << endl;
-            break;
+            domain = strtok(NULL, " ");
+            continue;
         }
         else cout << "Valid" << endl;
     
-        cout << "loading..."<<endl;
+        cout << "Loading..."<<endl;
         //Phan giai ten mien
 
         //const char* pdomain = domain.c_str();   //convert string to pcstr
@@ -77,14 +74,3 @@ int main()
     WSACleanup();
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
