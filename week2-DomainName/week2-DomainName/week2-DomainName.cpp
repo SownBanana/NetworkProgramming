6238c78669baa4b0d8115cf6540cb8cf6cb9be3f
Pham Hong Son 20173344 <son.ph173344@sis.hust.edu.vn>
2020-03-15 22:55:51 +0700

    multi domains

--- week2-DomainName/week2-DomainName/week2-DomainName.cpp
+++ week2-DomainName/week2-DomainName/week2-DomainName.cpp
@@ -2,25 +2,49 @@
 //
 
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <string>
+
 
 using namespace std;
 
 int main()
 {
-    string domain;
+    //Khoi tao winsock
+    WSADATA wsaData;
+    WORD wVersion = MAKEWORD(2, 2);
+    if (WSAStartup(wVersion, &wsaData)) {
+        cout << "Something went wrong :((";
+    }
+
+    addrinfo* info;         // Lay info
+    SOCKADDR_IN addr;
+
+    int count = 1;
+    //string input;
+    char* domains = new char[500];
+    char* domain;
     bool check_domain = true;
     bool check_dot = false;
+
+    cout << "Enter Some Domain Names (separate by space): ";
+    //getline(cin, input);
+    cin.getline(domains, 500);
+    //string to char*
+    
+    //strcpy(domains, input.c_str());
+    domain = strtok(domains, " ");
     //Kiem tra ten mien hop le
-    while (!check_domain || !check_dot) {
+    cout << "Checking Entered Domain Names..."<<endl;
+    cout << "================================" << endl;
+    while (domain != NULL) {
+        cout <<domain << " : ";
         check_domain = true;
         check_dot = false;
-        cout << "Enter A Domain: ";
-        cin >> domain;              //ten mien khong chua khoang trang
-        for (int i = 0; i < domain.size(); i++) {
+        for (int i = 0; i < strlen(domain); i++) {
             if (domain[i] == 46) check_dot = true;
             else if (!((48 <= domain[i] && domain[i] <= 57) ||
                 (65 <= domain[i] && domain[i] <= 90) ||
@@ -30,28 +54,28 @@ int main()
             }
         }
         if (!check_domain || !check_dot) {
-            cout << "It's not a domain! Reenter The Domain.\n";
+            cout << "Not Valid" << endl;
+            cout << "================================" << endl;
+            break;
         }
-    }
-    cout << "DNS : IP"<<endl;
-    //Khoi tao winsock
-    WSADATA wsaData;
-    WORD wVersion = MAKEWORD(2, 2);
-    if (WSAStartup(wVersion, &wsaData)) {
-        cout << "Something went wrong :((";
-    }
-
-    //Phan giai ten mien
-    addrinfo* info;         // Lay info
-    SOCKADDR_IN addr;     
-    const char* pdomain = domain.c_str();   //convert string to pcstr
-    int ret = getaddrinfo(pdomain, "http", NULL, &info);
-    if (ret == 0) {
-        memcpy(&addr, info->ai_addr, info->ai_addrlen);
-        cout <<domain<< " : " << inet_ntoa(addr.sin_addr);
-    }
-    else cout << "Can't find the domain name!";
+        else cout << "Valid" << endl;
     
+        cout << "loading..."<<endl;
+        //Phan giai ten mien
+
+        //const char* pdomain = domain.c_str();   //convert string to pcstr
+        int ret = getaddrinfo(domain, "http", NULL, &info);
+        if (ret == 0) {
+            memcpy(&addr, info->ai_addr, info->ai_addrlen);
+            cout << domain << " : " << inet_ntoa(addr.sin_addr) << endl;
+        }
+        else cout << domain << " : " << "Can't find the domain name!" << endl;
+        cout << "================================" << endl;
+        domain = strtok(NULL, " ");
+    }
+    delete[] domains;
+    WSACleanup();
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
