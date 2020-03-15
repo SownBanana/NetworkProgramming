cab8c7d65e8a89d768429cdb3a0a63931c65a530
Pham Hong Son 20173344 <son.ph173344@sis.hust.edu.vn>
2020-03-15 22:05:55 +0700

    1 domain a time

--- week2-DomainName/week2-DomainName/week2-DomainName.cpp
+++ week2-DomainName/week2-DomainName/week2-DomainName.cpp
@@ -1,9 +1,11 @@
 // week2-DomainName.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <iostream>
 #include <WinSock2.h>
-#include <typeinfo>
+#include <WS2tcpip.h>
 
 using namespace std;
 
@@ -12,16 +14,17 @@ int main()
     string domain;
     bool check_domain = true;
     bool check_dot = false;
+    //Kiem tra ten mien hop le
     while (!check_domain || !check_dot) {
+        check_domain = true;
+        check_dot = false;
         cout << "Enter A Domain: ";
         cin >> domain;              //ten mien khong chua khoang trang
-        //cout << domain;
         for (int i = 0; i < domain.size(); i++) {
-            //cout << typeid(domain[i]).name() << " ";
             if (domain[i] == 46) check_dot = true;
             else if (!((48 <= domain[i] && domain[i] <= 57) ||
                 (65 <= domain[i] && domain[i] <= 90) ||
-                (97 <= domain[i] && domain[i] <= 122))) {
+                (97 <= domain[i] && domain[i] <= 122) || domain[i] == 45)) {
                 check_domain = false;
                 break;
             }
@@ -30,7 +33,25 @@ int main()
             cout << "It's not a domain! Reenter The Domain.\n";
         }
     }
+    cout << "DNS : IP"<<endl;
+    //Khoi tao winsock
+    WSADATA wsaData;
+    WORD wVersion = MAKEWORD(2, 2);
+    if (WSAStartup(wVersion, &wsaData)) {
+        cout << "Something went wrong :((";
+    }
 
+    //Phan giai ten mien
+    addrinfo* info;         // Lay info
+    SOCKADDR_IN addr;     
+    const char* pdomain = domain.c_str();   //convert string to pcstr
+    int ret = getaddrinfo(pdomain, "http", NULL, &info);
+    if (ret == 0) {
+        memcpy(&addr, info->ai_addr, info->ai_addrlen);
+        cout <<domain<< " : " << inet_ntoa(addr.sin_addr);
+    }
+    else cout << "Can't find the domain name!";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
